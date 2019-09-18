9348449b7171d30f673cb49a14dd08adc901921d
Zeynep Hakguder <zhakguder@cse.unl.edu>
2019-09-18 10:33:19 -0500

    convert PosixPath to str

--- puzzlesolver/utils.py
+++ puzzlesolver/utils.py
@@ -3,18 +3,21 @@ from pathlib import Path
 
 def get_project_root() -> Path:
     """Returns project root folder."""
-    return Path(__file__).parent.parent
+    return str(Path(__file__).parent.parent)
+
 
 def divider(by):
     def divider_():
         x = 1
-        while x>=0.05:
+        while x >= 0.05:
             try:
                 x /= by
                 yield x
             except:
                 break
         return
+
     return divider_
+
 
 halver = divider(2)
