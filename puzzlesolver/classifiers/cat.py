43dd6b77dc041ca2d5cd0201ae6e37a29320fe3f
Zeynep Hakguder <zhakguder@cse.unl.edu>
2019-09-18 18:02:30 -0500

    minor

--- puzzlesolver/classifiers/cat.py
+++ puzzlesolver/classifiers/cat.py
@@ -50,7 +50,7 @@ class CatPredictor(keras.models.Model):
         for i, layer in enumerate(self.model_layers):
             output = layer(output)
             if i == n_layer - 2:
-                print(output)
+                print(output.numpy())
         return output
 
     @staticmethod
