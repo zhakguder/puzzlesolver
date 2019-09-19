6516a446065f84031607b8a97a566784d25fa21f
Zeynep Hakguder <zhakguder@cse.unl.edu>
2019-09-18 21:47:27 -0500

    delete parts of image

--- puzzlesolver/imageprocess/fileops.py
+++ puzzlesolver/imageprocess/fileops.py
@@ -1,4 +1,34 @@
 import cv2
+import numpy as np
+
 
 def read_image(image_path):
     return cv2.imread(image_path)
+
+
+def cut_image(image_path):
+    whole_image = read_image(image_path)
+    n_rows = whole_image.shape[0]
+    upper_zeros = int(np.ceil(n_rows / 2))
+    upper = np.copy(whole_image)
+    lower = np.copy(whole_image)
+    upper = _set_zeros(upper, range(upper_zeros, n_rows))
+    lower = _set_zeros(lower, range(0, upper_zeros))
+    return upper, lower
+
+
+def _set_zeros(image, row_range, col_range=None):
+    """
+    Set the given ranges to zero in the image. Sets those parts to white.
+
+   :param row_range:
+   :type row_range: range object
+   :param col_range:
+   :type col_range: range object
+    """
+    n_cols = image.shape[1]
+    if col_range is None:
+        col_range = range(0, n_cols)
+    for col in col_range:
+        image[row_range, col] = 0
+    return image
--- tests/test_imageprocess.py
+++ tests/test_imageprocess.py
@@ -1,40 +1,57 @@
-'''Tests for image processing'''
+"""Tests for image processing"""
 
 import unittest
-
-import cv2
-
-from click.testing import CliRunner
 from os import path
 
-from puzzlesolver.imageprocess import chaincode
+import cv2
+import numpy as np
+
+from puzzlesolver.imageprocess import chaincode, fileops
 from puzzlesolver.utils import get_project_root
 
 
 class TestImageProcess(unittest.TestCase):
-    '''Tests for imageprocess'''
+    """Tests for imageprocess"""
+
     def setUp(self):
         self.project_root = get_project_root()
-        self.img_path = path.join(self.project_root, 'assets', 'Castle.png')
+        self.img_path = path.join(self.project_root, "assets", "Castle.png")
         self.threshold = 254
-
+        self.dog_image = path.join(self.project_root, "data/train/dog.1526.jpg")
 
     def test_contours_to_chaincodes_returns_chaincodes(self):
-        '''Test that a chaincode is returned for every contour'''
-        test_contours = chaincode._contour(self.img_path,
-                                          threshold=self.threshold)
+        """Test that a chaincode is returned for every contour"""
+        test_contours = chaincode._contour(self.img_path, threshold=self.threshold)
         test_chaincodes = chaincode._contours_to_chaincodes(self.img_path)
         self.assertEqual(len(test_contours), len(test_chaincodes))
 
     def test_contour_to_chaincode_returns_chaincode(self):
-        '''Test that a chaincode is returned for a single contour'''
-        test_contour = chaincode._contour(self.img_path,
-                                          threshold=self.threshold)
+        """Test that a chaincode is returned for a single contour"""
+        test_contour = chaincode._contour(self.img_path, threshold=self.threshold)
         test_chaincode = chaincode._contour_to_chaincode(test_contour[0])
         self.assertLess(max(test_chaincode), 9)
         self.assertGreater(min(test_chaincode), 0)
 
     def test_contour_returns_something(self):
-        test_contour = chaincode._contour(self.img_path,
-                                          threshold=self.threshold)
+        test_contour = chaincode._contour(self.img_path, threshold=self.threshold)
         self.assertIsNotNone(test_contour)
+
+    @unittest.expectedFailure
+    def test_can_cut_image(self):
+        upper, lower = fileops.cut_image(self.dog_image)
+        self.assertIsNotNone(upper)
+        self.assertIsNotNone(lower)
+
+    def test_can_set_mask(self):
+        matrix = np.ones((10, 10))
+        n_all_entries = np.sum(matrix)
+        row_range = range(7, 10)
+        col_range = range(3, 6)
+        matrix = fileops._set_zeros(matrix, row_range, col_range)
+        n_zeros = len(row_range) * len(col_range)
+        self.assertEqual(np.sum(matrix), n_all_entries - n_zeros)
+
+    def test_can_cut_image(self):
+        upper, lower = fileops.cut_image(self.dog_image)
+        cv2.imwrite("upper.png", upper)
+        cv2.imwrite("lower.png", lower)
